e3abf3133cef939b59b6356e7ba88c7af3326533
guillaume-PC\guillaume <bonnet.guillaume.93@gmail.com>
2018-01-13 19:03:05 +0100

    using scss

--- freezer-app/src/app/aliment/aliment.component.ts
+++ freezer-app/src/app/aliment/aliment.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-aliment',
   templateUrl: './aliment.component.html',
-  styleUrls: ['./aliment.component.css']
+  styleUrls: ['./aliment.component.scss']
 })
 export class AlimentComponent implements OnInit {
 
